6e4f9305d4652753a4e2e0a0bfd003947b4c2566
Mike Beijen <m.f.beijen@student.tudelft.nl>
2021-05-06 15:27:44 +0200

    update calculation for T-Depth now that topic has been decided

--- ratedArgs.py
+++ ratedArgs.py
@@ -5,6 +5,9 @@ def processRatings(file):
     """
     Read the rated arguments from the CSV and sort them by prolificId and
     by metrics (TDepth, DQual, DInterp)
+    As for T-Depth, each argument should have the number of one of the four
+    subtopics it fits with best.
+
 
     :param file: the file to read the rated arguments from
     :return: argument ratings for all submissions sorted by prolificId and metric
@@ -16,10 +19,15 @@ def processRatings(file):
             regexRes = re.search("^([a-z0-9]{24}),([0-4]),([0-4]),([0-4]),(.*)$", line)
             if regexRes:
                 if not regexRes.group(1) in ratingsPerSubmission:
-                    ratingsPerSubmission[regexRes.group(1)] = {"TDepth": [], "DQual": [], "DIntrp": []}
-                ratingsPerSubmission[regexRes.group(1)]['TDepth'].append(int(regexRes.group(2)))
+                    ratingsPerSubmission[regexRes.group(1)] = {"DQual": [], "DIntrp": [], "TDepth-subtopic1": 0,
+                                                               "TDepth-subtopic2": 0, "TDepth-subtopic3": 0, "TDepth-subtopic4": 0}
+                ratingsPerSubmission[regexRes.group(1)]['TDepth-subtopic' + regexRes.group(2)] += 1
                 ratingsPerSubmission[regexRes.group(1)]['DQual'].append(int(regexRes.group(3)))
                 ratingsPerSubmission[regexRes.group(1)]['DIntrp'].append(int(regexRes.group(4)))
+
+    # Calculate T-Depth
+    for (submission, ratings) in ratingsPerSubmission.items():
+        ratingsPerSubmission[submission]['TDepth'] = avg([min(3, ratings["TDepth-subtopic" + str(i)]) for i in range(1, 5)])
 
     return ratingsPerSubmission
 
@@ -36,7 +44,7 @@ def writeAvgRatingsToCSV(file, ratings):
     file = open(file, "w")
     file.write("prolificId,avgTDepth,avgDQual,avgDIntrp\n")
     for (prolificId, pRatings) in ratings.items():
-        file.write(str(prolificId) + "," + str(avg(pRatings['TDepth'])) + "," + str(avg(pRatings['DQual'])) + "," + str(avg(pRatings['DIntrp'])) + "\n")
+        file.write(str(prolificId) + "," + str(pRatings['TDepth']) + "," + str(avg(pRatings['DQual'])) + "," + str(avg(pRatings['DIntrp'])) + "\n")
     file.close()
 
 
@@ -51,6 +59,6 @@ def avg(list):
 
 
 if __name__ == '__main__':
-    processedRatings = processRatings("/home/mike/git/bbt-analysis/data/in/argumentsRated.csv")
-    writeAvgRatingsToCSV("/home/mike/git/bbt-analysis/data/out/argumentsAvgRatings.csv", processedRatings)
+    processedRatings = processRatings("/home/mike/git/bbt-analysis/data/in/testdata/argumentsRated.csv")
+    writeAvgRatingsToCSV("/home/mike/git/bbt-analysis/data/out/testdata/argumentsAvgRatings.csv", processedRatings)
     print(processedRatings)
